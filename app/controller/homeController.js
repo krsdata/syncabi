b015d2879ace7c96dd7c2196bcc9606788cdfd57
Kundan Roy <kundan.roy@diaspark.com>
2017-05-01 15:10:50 +0530

    initial  setup

---
+++ app/controller/homeController.js
@@ -0,0 +1,12 @@
+﻿app.controller('homeController', function ($scope, $rootScope, commonService) {
+    init();
+
+    function init()
+    {
+        commonService.checkUserLoggedIn();
+        if ($rootScope.userID)
+        {
+            commonService.updatePath('login');
+        }
+    }
+});
